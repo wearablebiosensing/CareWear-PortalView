5bf6c366f16264daae2f8ce669bd018f635f16a4
sadhushehjar <45221445+sadhushehjar@users.noreply.github.com>
2023-07-11 09:28:45 -0400

    Magnetic Tiles Activity Starter

---
+++ CareWear-MagneticTilesActivity/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import firebase_admin
+from firebase_admin import credentials, storage, db
+import pandas as pd
+import io
+
+app = Flask(__name__)
+root = "/Users/shehjarsadhu/Desktop/UniversityOfRhodeIsland/Graduate/WBL/Project_Carehub/CareWear-PortalView/CareWear-MagneticTilesActivity/"
+cred = credentials.Certificate(root + 'carewear-77d8e-b0c3a74e907c.json')
+firebaseConfig = {
+  "apiKey": "AIzaSyDyjHLuokjuGEPr3HOSsX8FP16qxyS62W8",
+  "authDomain": "carewear-77d8e.firebaseapp.com",
+  "databaseURL": "https://carewear-77d8e-default-rtdb.firebaseio.com",
+  "projectId": "carewear-77d8e",
+  "storageBucket": "carewear-77d8e.appspot.com",
+  "messagingSenderId": "683558385369",
+  "appId": "1:683558385369:web:1d729eff041a05d547b0c8"
+}
+firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://carewear-77d8e-default-rtdb.firebaseio.com/'
+})
+ref = db.reference('/sensors_message')  # Path to your sensor data node in the database
+
+
+@app.route('/')
+def home():
+ 
+    return render_template("home.html")
